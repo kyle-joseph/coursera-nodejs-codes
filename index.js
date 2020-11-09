113c023dec02b491c338882bb9c7a9847c3bdca9
Kyle Joseph Timajo <timajokyle24@gmail.com>
2020-11-09 23:15:19 +0800

    Node Express Init

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+
+app.use((req, res, next) => {
+    
+});
