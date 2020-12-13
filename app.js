64a8cc3dbb05f7ac1b0272075a22effc586661d4
Kyle Joseph Timajo <timajokyle24@gmail.com>
2020-12-13 18:09:57 +0800

    Passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var session = require("express-session");
 var FileStore = require("session-file-store")(session);
+var passport = require("passport");
+var authenticate = require("./authenticate");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -36,6 +38,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser("12345-67890-09876-54321"));
+
 app.use(
     session({
         name: "session-id",
@@ -46,24 +49,21 @@ app.use(
     })
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 function auth(req, res, next) {
-    console.log(req.session);
+    console.log(req.user);
 
-    if (!req.session.user) {
+    if (!req.user) {
         var err = new Error("You are not authenticated!");
-        err.status = 401;
-        return next(err);
+        err.status = 403;
+        next(err);
     } else {
-        if (req.session.user === "authenticated") {
-            next();
-        } else {
-            var err = new Error("You are not authenticated!");
-            err.status = 403;
-            return next(err);
-        }
+        next();
     }
 }
 
