3826d1ca12c0381bb5b5c1e58de85ad37b6c5166
Kyle Joseph Timajo <timajokyle24@gmail.com>
2021-01-17 17:00:21 +0800

    Passport JWT

--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -1,90 +1,114 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
-const Promotions = require('../models/promotions');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const authenticate = require("../authenticate");
+const Promotions = require("../models/promotions");
 
 const promoRouter = express.Router();
 
 promoRouter.use(bodyParser.json());
 
-
 // /promotions route
-promoRouter.route('/')
-.get((req,res,next) => {
-  Promotions.find({})
-    .then((promo) =>{
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.json(promo);
-    }, (err) => next(err))
-    .catch((err) => next(err));
-})
+promoRouter
+    .route("/")
+    .get((req, res, next) => {
+        Promotions.find({})
+            .then(
+                (promo) => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "application/json");
+                    res.json(promo);
+                },
+                (err) => next(err)
+            )
+            .catch((err) => next(err));
+    })
 
-.post((req, res, next) => {
-  Promotions.create(req.body)
-    .then((promo) => {
-      console.log('Promotion created ', promo);
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.json(promo);
-    }, (err) => next(err))
-    .catch((err) => next(err));
-})
+    .post(authenticate.verifyUser, (req, res, next) => {
+        Promotions.create(req.body)
+            .then(
+                (promo) => {
+                    console.log("Promotion created ", promo);
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "application/json");
+                    res.json(promo);
+                },
+                (err) => next(err)
+            )
+            .catch((err) => next(err));
+    })
 
-.put((req, res, next) => {
-  res.statusCode = 403;
-  res.end('PUT operation not supported on /promotions');
-})
- 
-.delete((req, res, next) => {
-  Promotions.remove({})
-    .then((resp) => {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.json(resp);
-    }, (err) => next(err))
-    .catch((err) => next(err));
-});
+    .put(authenticate.verifyUser, (req, res, next) => {
+        res.statusCode = 403;
+        res.end("PUT operation not supported on /promotions");
+    })
+
+    .delete(authenticate.verifyUser, (req, res, next) => {
+        Promotions.remove({})
+            .then(
+                (resp) => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "application/json");
+                    res.json(resp);
+                },
+                (err) => next(err)
+            )
+            .catch((err) => next(err));
+    });
 
 // /:promoId route
-promoRouter.route('/:promoId')
-.get((req,res,next) => {
-  Promotions.findById(req.params.promoId)
-    .then((promo) => {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.json(promo);
-    }, (err) => next(err))
-    .catch((err) => next(err));
-    
-})
+promoRouter
+    .route("/:promoId")
+    .get((req, res, next) => {
+        Promotions.findById(req.params.promoId)
+            .then(
+                (promo) => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "application/json");
+                    res.json(promo);
+                },
+                (err) => next(err)
+            )
+            .catch((err) => next(err));
+    })
 
-.post((req, res, next) => {
-  res.statusCode = 403;
-  res.end('POST operation not supported on /promotions/'+ req.params.promoId);
-})
+    .post(authenticate.verifyUser, (req, res, next) => {
+        res.statusCode = 403;
+        res.end(
+            "POST operation not supported on /promotions/" + req.params.promoId
+        );
+    })
 
-.put((req, res, next) => {
-  Promotions.findByIdAndUpdate(req.params.promoId, {
-      $set: req.body
-    }, {new:true})
-    .then((promo) => {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.json(promo);
-    }, (err) => next(err))
-    .catch((err) => next(err));
-})
+    .put(authenticate.verifyUser, (req, res, next) => {
+        Promotions.findByIdAndUpdate(
+            req.params.promoId,
+            {
+                $set: req.body,
+            },
+            { new: true }
+        )
+            .then(
+                (promo) => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "application/json");
+                    res.json(promo);
+                },
+                (err) => next(err)
+            )
+            .catch((err) => next(err));
+    })
 
-.delete((req, res, next) => {
-  Promotions.findByIdAndRemove(req.params.promoId)
-    .then((resp) => {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.json(resp);
-    }, (err) => next(err))
-    .catch((err) => next(err));
-});
+    .delete(authenticate.verifyUser, (req, res, next) => {
+        Promotions.findByIdAndRemove(req.params.promoId)
+            .then(
+                (resp) => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "application/json");
+                    res.json(resp);
+                },
+                (err) => next(err)
+            )
+            .catch((err) => next(err));
+    });
 
 module.exports = promoRouter;
